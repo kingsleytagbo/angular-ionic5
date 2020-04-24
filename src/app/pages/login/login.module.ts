73f563d693e7fa025ad64ee96dca859a992cd7b0
ktagbo <kingsleytagbo@gmail.com>
2020-04-24 18:45:31 -0500

    Footer

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -5,13 +5,15 @@ import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login';
 import { LoginPageRoutingModule } from './login-routing.module';
+import { FooterModule } from '../../components/footer/footer.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    LoginPageRoutingModule
+    LoginPageRoutingModule,
+    FooterModule
   ],
   declarations: [
     LoginPage,
